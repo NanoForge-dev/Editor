ac5d02ec7471dc53fd43dbc1bd1fd48c3b5b853d
bill <timothe.jacquot@epitech.eu>
2025-09-09 23:56:26 +0200

    feat: create header with tabs and menu
    # Conflicts:
    #	src/components/Menu/MenuButton.svelte
    #	src/routes/Home.svelte

---
+++ src/components/Tabs/TabBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import TabComponent from './Tab.svelte';
+  import { type Tab, tabTypes } from './Tab';
+
+  let tabs: Tab[] = $state([
+    { type: tabTypes[0], title: 'Game' },
+    { type: tabTypes[2], title: 'Player.obj' },
+    { type: tabTypes[1], title: 'main.ts' },
+    { type: tabTypes[3], title: 'original-sound-track.wav' },
+  ]);
+  let selected = $state(0);
+</script>
+
+<div class="flex gap-1 h-8 w-full">
+  {#each tabs as tab, index (tab)}
+    <TabComponent
+      {tab}
+      selected={selected === index}
+      closable={tab.type.name !== 'main'}
+      onSelect={() => (selected = index)}
+      onClose={() => tabs.splice(index, 1)}
+    />
+  {/each}
+</div>
