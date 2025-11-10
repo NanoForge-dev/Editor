ab1d7ab175b9e0381f2809adcf992dcf0227cbe7
Exelo <exelo.corp@gmail.com>
2025-11-10 13:53:12 +0100

    feat: make API base URL configurable via environment variables

--- src/modules/http/client.ts
+++ src/modules/http/client.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '../../utils/http-client';
 import { AuthMiddleware } from './middlewares/auth.middleware';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
-export const client = new HttpClient('http://localhost:3000').useMiddlewares(
+export const client = new HttpClient(import.meta.env.VITE_API_BASE_URL).useMiddlewares(
   AuthMiddleware,
   LoggerMiddleware,
 );
