06a8b2e819d08c2b02fc678fa7e312bc438379cb
unknown <timothe.jacquot@epitech.eu>
2025-11-19 23:19:54 +0100

    fix: add item type on content browser

--- src/components/Panel/Widget.svelte
+++ src/components/Panel/Widget.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {widgetsTypes} from "../Widget/widgets";
+  import { widgetsTypes } from '../Widget/widgets';
 
   interface Props {
     size?: number;
@@ -8,17 +8,15 @@
 
   let { size, id }: Props = $props();
 
-  const widgetsMap = Object.fromEntries(
-          widgetsTypes.map((w) => [w.id, w.component])
-  );
+  const widgetsMap = Object.fromEntries(widgetsTypes.map((w) => [w.id, w.component]));
 
   let WidgetComponent = $derived(id ? widgetsMap[id] : null);
 </script>
 
 <div
-      class="bg-neutral-800 w-full h-full rounded-md flex items-center justify-center text-white border border-neutral-600 overflow-hidden"
-      style:flex-basis={size ? `${size}%` : '100%'}
-      data-widget-id={id}
+  class="bg-neutral-800 w-full h-full rounded-md flex items-center justify-center text-white border border-neutral-600 overflow-hidden"
+  style:flex-basis={size ? `${size}%` : '100%'}
+  data-widget-id={id}
 >
   {#if WidgetComponent}
     <WidgetComponent />
