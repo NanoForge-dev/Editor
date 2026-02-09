3e347f82507af6b0f72c51f48219d2abb1abb661
Exelo <exelo.corp@gmail.com>
2026-02-09 16:01:11 +0100

    ci: add pre-release (#39)
    * chore: upgrade packages
    
    * ci: add pre-release
    
    * chore: add coverage and fix prettierignore

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+import prettierConfig from '@nanoforge-dev/utils-prettier-config';
+
+export default {
+  ...prettierConfig,
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-svelte'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+  importOrder: ['^$app/(.*)$', '^lib/(.*)$', '^~/(.*)$', '^[./]'],
+  singleQuote: true,
+};
