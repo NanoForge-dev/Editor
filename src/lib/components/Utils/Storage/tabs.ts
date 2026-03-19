8ba42ee5fa5380e730091be04caacab51a9808de
bill <timothe.jacquot@epitech.eu>
2026-03-19 20:34:02 +0100

    feat: create project list cache on project-loader

--- src/lib/components/Utils/Storage/tabs.ts
+++
@@ -1,9 +0,0 @@
-import { getDB } from '$lib/components/Utils/Storage/db';
-
-export async function saveTab(fileName: string, content: string) {
-  const db = await getDB();
-  const tx = db.transaction('tabs', 'readwrite');
-  const store = tx.objectStore('tabs');
-  await store.put({ id: fileName, content, lastModified: Date.now() });
-  await tx.done;
-}
