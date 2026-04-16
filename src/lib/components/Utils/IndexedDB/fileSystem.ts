6c9d1ebe3f917efed0b1771c248ccfda6eec4abd
bill <timothe.jacquot@epitech.eu>
2026-04-16 10:06:38 +0200

    fix: delete old function and variables link to indexedDB

--- src/lib/components/Utils/IndexedDB/fileSystem.ts
+++
@@ -1,9 +0,0 @@
-import { getDB } from '$lib/components/Utils/IndexedDB/db';
-
-export async function loadFile(fileName: string): Promise<string> {
-  const db = await getDB();
-  const tx = db.transaction('files', 'readonly');
-  const store = tx.objectStore('files');
-  const file = await store.get(fileName);
-  return file?.content || '';
-}
