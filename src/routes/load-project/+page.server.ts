cdff6ba9a6a7f737bb302e18a85c8514642ef806
Tchips46 <leo.out46@gmail.com>
2026-04-20 00:42:50 +0900

    feat(save handle): import component manifest

--- src/routes/load-project/+page.server.ts
+++ src/routes/load-project/+page.server.ts
@@ -69,7 +69,6 @@ export const actions = {
     }
     absoluteProjectPath = path.resolve(absoluteProjectPath);
 
-    console.log(locals.session.data.path);
     const session = locals.session;
 
     await session.setData({ path: absoluteProjectPath });
