6666d3d2dcf884238beb9a374aacaa86bb1d37c8
Tchips46 <leo.out46@gmail.com>
2026-04-28 14:17:27 +0900

    fix: strip env prefix

--- src/lib/loader/server/env.ts
+++ src/lib/loader/server/env.ts
@@ -4,14 +4,19 @@ const PREFIX_CLIENT = 'NANOFORGE_CLIENT_';
 const PREFIX_SERVER = 'NANOFORGE_SERVER_';
 const PREFIX = 'NANOFORGE_';
 
-export const getGameEnv = (side: 'client' | 'server') => {
+export const getGameEnv = (side: 'client' | 'server'): { [key: string]: string | undefined } => {
   return Object.fromEntries(
     Object.entries(env)
-      .filter(
-        ([key]) =>
-          key.startsWith(PREFIX) ||
-          key.startsWith(side === 'client' ? PREFIX_CLIENT : PREFIX_SERVER),
-      )
-      .map(([key, value]) => [key.replace(PREFIX, ''), value]),
+      .map(([key, value]) => {
+        if (side === 'server' && key.startsWith(PREFIX_SERVER)) {
+          return [key.slice(PREFIX_SERVER.length), value];
+        } else if (side === 'client' && key.startsWith(PREFIX_CLIENT)) {
+          return [key.slice(PREFIX_CLIENT.length), value];
+        } else if (key.startsWith(PREFIX)) {
+          return [key.slice(PREFIX.length), value];
+        }
+        return undefined;
+      })
+      .filter((e) => e !== undefined),
   );
 };
