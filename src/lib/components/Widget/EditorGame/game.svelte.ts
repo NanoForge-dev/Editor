6ab3506381d7c4cb98838a4b08a9ea6e6a51613e
Tchips46 <leo.out46@gmail.com>
2026-05-28 10:10:33 +0900

    fix(game): remove old needed game start values like save

--- src/lib/components/Widget/EditorGame/game.svelte.ts
+++ src/lib/components/Widget/EditorGame/game.svelte.ts
@@ -1,18 +1,3 @@
-import { SvelteMap } from 'svelte/reactivity';
-import { type Writable, writable } from 'svelte/store';
-
-import type { Save } from '@utils/types';
-
-export const mainModule: Writable<any> = writable(undefined);
-export const env: Writable<Record<string, string>> = writable({});
-export const files: Writable<Map<string, string>> = writable(new SvelteMap());
-export const save: Writable<Save> = writable({
-  libraries: [],
-  entities: [],
-  components: [],
-  systems: [],
-});
-
 export enum GameState {
   INIT_STATE = 0,
   RELOAD_FROM_SERVER,
--- src/lib/components/Widget/EntityInspector/component-creator.ts
+++ src/lib/components/Widget/EntityInspector/component-creator.ts
@@ -1,18 +1,14 @@
-import { get } from 'svelte/store';
-
 import { useProject } from '$lib/client/project';
-import { save } from '$lib/components/Widget/EditorGame/game.svelte';
 
 import type { SaveComponent } from '@utils/types';
 
 export async function createDefaultComponent(
   componentName: string,
 ): Promise<[string, Record<string, any>]> {
-  const savedComponent: SaveComponent[] = get(save).components;
+  const { save, packages } = useProject();
+  const savedComponent: SaveComponent[] = save.save.components;
   const componentToCreate = savedComponent?.find((c) => c.name === componentName);
   if (!componentToCreate) throw Error(`Can't create ${componentName}: not found`);
-
-  const { packages } = useProject();
 
   const componentManifest = packages.getComponentManifest(componentToCreate.path);
 
