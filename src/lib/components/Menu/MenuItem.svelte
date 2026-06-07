8c768e40353c768dd19af64aa77449da43167d5f
Exelo <exelo.corp@gmail.com>
2026-06-07 15:36:08 +0900

    feat: add docs buttons in menu (#149)
    * feat: add docs buttons in menu
    
    * ci: add new env to tests ci
    
    * ci: fix env issue
    
    * ci: fix env use vars at release ci
    
    * fix: open links in new tab

--- src/lib/components/Menu/MenuItem.svelte
+++ src/lib/components/Menu/MenuItem.svelte
@@ -4,20 +4,32 @@
 
   interface Props {
     icon?: string;
+    link?: string;
     children?: Snippet;
     onClick?: () => void;
   }
-  let { icon, children, onClick }: Props = $props();
+  let { icon, link, children, onClick }: Props = $props();
 </script>
 
 <div>
   <DropdownMenuItem class="p-0 w-full">
-    <button
-      class="flex cursor-pointer items-center gap-2 px-3 py-2 text-white text-xs hover:bg-neutral-600 w-full"
-      onclick={onClick}
-    >
-      <span class="{icon || 'opacity-0'} w-4 h-4 text-xs text-neutral-200 rounded-none"></span>
-      {@render children?.()}
-    </button>
+    {#if link}
+      <a
+        href={link}
+        target="_blank"
+        class="flex cursor-pointer items-center gap-2 px-3 py-2 text-white text-xs hover:bg-neutral-600 w-full"
+      >
+        <span class="{icon || 'opacity-0'} w-4 h-4 text-xs text-neutral-200 rounded-none"></span>
+        {@render children?.()}
+      </a>
+    {:else}
+      <button
+        class="flex cursor-pointer items-center gap-2 px-3 py-2 text-white text-xs hover:bg-neutral-600 w-full"
+        onclick={onClick}
+      >
+        <span class="{icon || 'opacity-0'} w-4 h-4 text-xs text-neutral-200 rounded-none"></span>
+        {@render children?.()}
+      </button>
+    {/if}
   </DropdownMenuItem>
 </div>
