c371fde36adf963457b7dd11bfca14dfe1cd5d04
Tchips46 <leo.out46@gmail.com>
2026-06-09 15:41:12 +0900

    feat(entity-inspector): show differents enum values as selectable list

--- src/lib/components/Widget/entity-inspector/component-param-row.svelte
+++ src/lib/components/Widget/entity-inspector/component-param-row.svelte
@@ -2,6 +2,7 @@
   import type { ComponentParamHandle } from '$lib/client/ecs';
   import { Input } from '$lib/components/ui/input';
   import { TristateSwitch } from '$lib/components/ui/tristate-switch';
+  import { Select, SelectContent, SelectItem, SelectTrigger } from '$lib/components/ui/select';
 
   interface Props {
     handle: ComponentParamHandle;
@@ -23,7 +24,32 @@
   <div class="text-neutral-200 text-sm">{$param.name}</div>
 
   {#if $param.type === 'string'}
-    <Input type="text" bind:value onchange={handleChange} />
+    {#if $param.enum}
+      <Select type="single" bind:value>
+        <SelectTrigger class="w-full">{value}</SelectTrigger>
+        <SelectContent>
+          {#if Array.isArray($param.enum)}
+            {#if !!$param.enum.length}
+              {#each $param.enum as opt (opt)}
+                <SelectItem value={opt}>{opt}</SelectItem>
+              {/each}
+            {:else}
+              <div class="px-4 py-2 text-muted-foreground italic">No options</div>
+            {/if}
+          {:else}
+            {#if !!$param.enum.length}
+              {#each Object.entries($param.enum) as [displayOpt, realOpt] (displayOpt)}
+                <SelectItem value={realOpt}>{displayOpt}</SelectItem>
+              {/each}
+            {:else}
+              <div class="px-4 py-2 text-muted-foreground italic">No options</div>
+            {/if}
+          {/if}
+        </SelectContent>
+      </Select>
+    {:else}
+      <Input type="text" bind:value onchange={handleChange} />
+    {/if}
   {:else if $param.type === 'number'}
     <Input type="number" bind:value onchange={handleChange} />
   {:else if $param.type === 'boolean'}
